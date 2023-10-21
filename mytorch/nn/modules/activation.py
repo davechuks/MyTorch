a0416f15a90dd3852baa9642104d9a5ebb3f9d5c
davechuks <bytophyte@gmail.com>
2023-10-21 16:10:48 +0100

    redesign activation functions to inherit from Layer.

--- mytorch/nn/modules/activation.py
+++ mytorch/nn/modules/activation.py
@@ -3,33 +3,32 @@ import numpy as np
 
 class Identity:
     def forward(self, Z):
-        self.A = Z
         return Z
 
-    def backward(self):
-        return np.ones_like(self.A, dtype=np.float32)
-    
+    def backward(self, dLdA):
+        return dLdA
+
 
 class Sigmoid:
     def forward(self, Z):
         self.A = 1 / (1 + np.exp(-Z))
         return self.A
-    
-    def backward(self):
-        return self.A(1 - self.A)
-    
+
+    def backward(self, dLdA):
+        return dLdA * self.A * (1 - self.A)
+
 class Tanh:
     def forward(self, Z):
         self.A = np.tanh(Z)
         return self.A
-    
-    def backward(self):
-        return 1 - self.A ** 2
-    
+
+    def backward(self, dLdA):
+        return dLdA * (1 - self.A ** 2)
+
 class ReLU:
     def forward(self, Z):
         self.A = np.maximum(0, Z)
         return self.A
-    
-    def backward(self):
-        return np.where(self.A > 0, 1, 0)
+
+    def backward(self, dLdA):
+        return dLdA * np.where(self.A > 0, 1, 0)
